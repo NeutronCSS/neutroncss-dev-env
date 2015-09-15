d1c909ba4bc7a05b17ef6dfc7ec3e2dfecf8d7de
Christopher Muller <inbox@christophermuller.net>
2015-09-15 22:20:47 +1000

    Removed clone command.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
-var git = require('gulp-git'); 
 
 // SASS
 gulp.task('sass', function() {
@@ -8,13 +7,6 @@ gulp.task('sass', function() {
         // .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./tests/css/'));
 });
-
-//Clone Neutron Repos
-gulp.task('clone', function(){
-	git.clone('https://github.com/NeutronCSS/neutroncss.git', {cwd: "./neutroncss/"});
-	git.clone('https://github.com/NeutronCSS/neutron-tests.git', {cwd: "./neutron-tests/"});
-});
-
 
 //SASS Watch task
 gulp.task('watch',function() {
