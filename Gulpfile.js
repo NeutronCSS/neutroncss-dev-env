af7f6038fcf694344852f1061f3eddc8515ced93
Christopher Muller <inbox@christophermuller.net>
2015-10-08 21:30:06 +1100

    Fixing dev env settings

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // SASS
 gulp.task('sass', function() {
     return sass('tests/**/*.scss', {style: 'expanded'})
         // .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./tests/css/'));
+         .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+       .pipe(gulp.dest('./tests/'));
 });
 
 //SASS Watch task
-gulp.task('watch',function() {
+gulp.task('watch', ['sass'], function() {
     gulp.watch('neutroncss/**/*.scss',['sass']);
     gulp.watch('tests/**/*.scss',['sass']);
 });
 
 //Default task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['watch']);
